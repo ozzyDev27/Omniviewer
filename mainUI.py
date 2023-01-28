c38c75a9734ed326f6ba5084e0e529d2445ae4ef
ultrablob <neelgillshah@gmail.com>
2023-01-28 18:26:26 -0500

    migrated the basic ui to pyqt5

---
+++ mainUI.py
@@ -0,0 +1,34 @@
+import sys, os
+from PyQt5 import QtCore, QtGui, QtWidgets
+from new_gui import Ui_MainWindow
+
+
+class Omniviewer(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Omniviewer, self).__init__()
+
+        # Set up the user interface from Designer.
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.actionOpen.triggered.connect(self.pick_file)
+
+    def test_activation(self):
+        print("ohio")
+        
+    def pick_file(self):
+        fileName = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Pick File", "~/", "Anything (*.*)"
+        )[0]
+
+        self.setWindowTitle(f"Omniviewer | {os.path.basename(fileName)}")
+        
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    omniviewer = Omniviewer()
+    omniviewer.show()
+    sys.exit(app.exec_())
